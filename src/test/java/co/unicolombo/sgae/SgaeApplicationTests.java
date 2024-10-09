78bd1a3f7876c89f9bb3c8f8a10680420480b233
Jesus Castro <jesusdavidcastror30@gmail.com>
2024-10-09 16:00:24 -0500

    Primer commit

---
+++ src/test/java/co/unicolombo/sgae/SgaeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicolombo.sgae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
